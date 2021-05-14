9e08a1c074192b1e6585a69e0b02d003bf6b67f2
Tamal Karmakar <giet13ee080@gmail.com>
2021-05-14 21:19:22 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src",
+    version = "0.0.1",
+    description = "Its a wine Quality packages",
+    author = "tamal",
+    packages = find_packages(),
+    license = "MIT"
+)
+
+
--- src/get_data.py
+++ src/get_data.py
@@ -1,11 +1,12 @@
-## read parameters
-## process
-## return dataframe
+# read parameters
+# process
+# return dataframe
 
-import os
+# import os
 import yaml
 import pandas as pd
 import argparse
+
 
 def read_params(config_path):
     with open(config_path) as yaml_file:
@@ -15,16 +16,14 @@ def read_params(config_path):
 
 def get_data(config_path):
     config = read_params(config_path)
-    #print(config)
+    # print(config)
     data_path = config["data_source"]["s3_source"]
-    df = pd.read_csv(data_path,sep=",",encoding='utf-8')
+    df = pd.read_csv(data_path, sep=",", encoding='utf-8')
     return df
+
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config",default="params.yaml")
+    args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
     get_data(config_path=parsed_args.config)
-
-
-
